ce76336bcd429b03ff852a39f17043c2b68bfbd9
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-20 16:55:56 +0700

    support language change from menu

--- src/components/DialogForm.vue
+++ src/components/DialogForm.vue
@@ -4,7 +4,7 @@
         <div class="modal-content portal-area fa-portal-area">
             <div class="modal-header">
                 <slot name="header"></slot>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close btn-close" data-dismiss="modal"></button>
             </div>
             <div class="entry-dialog-layer">
                 <slot></slot>
